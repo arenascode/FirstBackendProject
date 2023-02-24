8b77b319591039de9d6fadbf12b996350c64636c
Miguel Arenas <migue.arenas91@gmail.com>
2023-02-24 17:48:48 -0300

    Post for add a Cart Done

--- src/manager/CartManager.js
+++ src/manager/CartManager.js
@@ -17,20 +17,19 @@ class CartManager {
 
   // Methods
 
-  async addCart({productCode}) {
+  async addCart(productToAdd) {
 
     const routeProducts = './src/db/products.json';
 
     const readProducts = JSON.parse(await fs.promises.readFile(routeProducts, "utf-8"))
 
-    const productExist = readProducts.find(e => e.code === productCode)
+    const productExist = readProducts.find(e => e.code == productToAdd.code)
 
     console.log((`line 27 says: ${JSON.stringify(productExist)}`));
 
     const carts = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
     console.log(`line 31 says: ${JSON.stringify(carts)}`);
     
-    // const indexProductInCart = carts.find(e => e.products.findIndex(p => p.idProduct === productExist.id))
     const indexProductInCart = carts.find((e) =>
       e.products[0].idProduct == productExist.id)
     ;
@@ -49,14 +48,6 @@ class CartManager {
     } else {
       indexProductInCart.products[0].quantity += 1
       console.log(` LINEA 50 Says: ${JSON.stringify(indexProductInCart)}`);
-      // review this part
-      // const index = carts.findIndex(e => e.products == productInCart.idProduct)
-      // console.log(`line 53: ${index}`);
-      // const productToUpdate = { ...cart.products[index] }
-      // console.log(`line 55: ${JSON.stringify(productToUpdate)}`);
-      // productToUpdate.quantity= + 1
-      // const updateProducts = [...cart.products.slice(0,index), productToUpdate, cart.products.slice(index + 1)]
-      // cart.products.push(updateProducts)
     }
     await fs.promises.writeFile(this.path, JSON.stringify(carts))
   }
@@ -65,17 +56,19 @@ class CartManager {
     const readJson = JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
     return readJson
   }
+
+
 }
 
-const instanceTest = new CartManager('./Carts.json');
+// const instanceTest = new CartManager('./Carts.json');
 
-(async () => {
-  await instanceTest.addCart({productCode: "001"})
-  await instanceTest.addCart({productCode: "002"});
-  await instanceTest.addCart({productCode: "003"})
-  // await instanceTest.addCart({productCode: "003"});
+// (async () => {
+//   await instanceTest.addCart({productCode: "001"})
+//   await instanceTest.addCart({productCode: "002"});
+//   await instanceTest.addCart({productCode: "003"})
+//   // await instanceTest.addCart({productCode: "003"});
 
-  console.log(await instanceTest.showCart())
-})()
+//   console.log(await instanceTest.showCart())
+// })()
 
 export default CartManager
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -4,21 +4,29 @@ import CartManager from "../manager/CartManager.js";
 const routerCart = Router()
 const cartsManager = new CartManager('./src/db/carts.json')
 
-routerCart.get('/', (req, res) => {
-  // develop logic to show all products of ca
+routerCart.get('/:cid', async (req, res) => {
+  const idProduct = req.params.cid
+
+  if (!idProduct) {
+    console.log('This Cart doesnt exists');
+  } 
+  const showCart = await cartsManager.showCart()
+  // develop logic to show all products of cart
+  res.json(showCart)
 });
 
 
-// routerCart.post('/', async (req, res) => {
-//   const cartToAdd = req.body
-//   await cartsManager.addCart(cartToAdd)
-//   res.json(`the ${JSON.stringify(cartToAdd)} was added succesfull`)
-// // develop logic to create a new cart
-// })
-
-routerCart.get('/:cid', (req, res) => {
-  // Develop logic to show products of the cart id in the argument
+routerCart.post('/', async (req, res) => {
+  const productToCart = req.body
+  console.log(`Line 21 OF Cart.Router ${productToCart}`);
+  await cartsManager.addCart(productToCart)
+  res.json(`the ${JSON.stringify(productToCart)} was added succesfull`)
+// develop logic to create a new cart
 })
+
+// routerCart.get('/:cid', (req, res) => {
+//   // Develop logic to show products of the cart id in the argument
+// })
 
 routerCart.post('/:cid/product/:pid', (req, res) => {
   // Develop logic to add a product in a object to array with only id and quantity
