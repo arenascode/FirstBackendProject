23654f1f2bd90d5b42f40ec72fef7aa5d5e46897
Miguel Arenas <migue.arenas91@gmail.com>
2023-07-20 20:19:20 -0500

    adjust in cluster

--- src/main.js
+++ src/main.js
@@ -4,25 +4,27 @@ import { winstonLogger } from "./utils/logger.js"
 import { cpus } from "node:os";
 import { PORT } from "./config/PortServer.config.js";
 
-if (cluster.isPrimary) {
-  winstonLogger.info(`I'm primary process. My ID is ${process.pid}`)
+// if (cluster.isPrimary) {
+//   winstonLogger.info(`I'm primary process. My ID is ${process.pid}`)
 
-for (let i = 0; i < cpus().length; i++) {
-  cluster.fork()  
-}
+// for (let i = 0; i < cpus().length; i++) {
+//   cluster.fork()  
+// }
 
-  cluster.on('exit', worker => {
-    winstonLogger.info(`Process ${worker.process.pid} Ended`) 
-    cluster.fork()
-  })
-}
+//   cluster.on('exit', worker => {
+//     winstonLogger.info(`Process ${worker.process.pid} Ended`) 
+//     cluster.fork()
+//   })
+// }
 
-let connectedServer
-if (cluster.isWorker) {
-  winstonLogger.info(`I'm worker process. My ID is ${process.pid}`);
-  connectedServer = app.listen(PORT, '0.0.0.0', () => {
-    winstonLogger.info(`App Connected To Port ${connectedServer.address().port}`)
-  })
-}
-
+// let connectedServer
+// if (cluster.isWorker) {
+//   winstonLogger.info(`I'm worker process. My ID is ${process.pid}`);
+//   connectedServer = app.listen(PORT, '0.0.0.0', () => {
+//     winstonLogger.info(`App Connected To Port ${connectedServer.address().port}`)
+//   })
+// }
+const connectedServer = app.listen(PORT, '0.0.0.0', () => {
+  winstonLogger.info(`App Connected To Port ${connectedServer.address().port}`)
+})
 export default connectedServer
